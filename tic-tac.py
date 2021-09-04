2c42a8b02b0a31ba26d7d7962324be8e6f4c87b9
Loist56 <59015054+Loist56@users.noreply.github.com>
2021-09-04 13:43:53 +0700

    User input for turn 1

--- tic-tac.py
+++ tic-tac.py
@@ -4,10 +4,13 @@ print("Hello welcome to tic tac toe, player 1 you will use X and player 2 you wi
 
 import random
 n = random.randint(1,2)
+firstplay = ""
 if n == 1:
     print("Player 1 you will go first")
+    firstplay = "X"
 else:
     print("Player 2 you will go first")
+    firstplay ="O"
 
 #building the grid
 grid = [[] for x in range(0,3)]
@@ -18,4 +21,16 @@ for r in grid:
     for c in r:
         print(c,end = " ")
     print()
+
+#play game
+
+#round1
+firstplay_x = input("First player enter a number between 0-2 for x coordinate:")
+if firstplay_x > 2:
+    firstplay_x = input("incorrect please enter a number between 0-2:")
+firstplay_y = input("First player enter a number between 0-2 for y coordinate:")
+if firstplay_y > 2:
+    firstplay_y = input("incorrect please enter a number between 0-2:")
+
+
 
