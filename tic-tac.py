ee8e6f5792245389c164cc9fce4c091892f95da5
Loist56 <59015054+Loist56@users.noreply.github.com>
2021-09-04 13:29:00 +0700

    starting player

--- tic-tac.py
+++ tic-tac.py
@@ -1,1 +1,12 @@
 print("Hello welcome to tic tac toe, player 1 you will use X and player 2 you will use O")
+
+#find starting player 
+
+import random
+n = random.randint(1,2)
+if n == 1:
+    print("Player 1 you will go first")
+else:
+    print("Player 2 you will go first")
+
+
